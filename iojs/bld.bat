0c3149ceefd439936ef0d38f1bc73f8fbf4a5b75
Travis Swicegood <development@domain51.com>
2015-01-20 17:26:45 -0800

    Restructure how this directory is moved around for Windows

--- iojs/bld.bat
+++ iojs/bld.bat
@@ -1,6 +1,9 @@
-move node_modules %PREFIX%\Scripts
+mkdir %PREFIX%\Scripts
+copy bin\npm.cmd %PREFIX%\Scripts
+mkdir %PREFIX%\Scripts\node_modules
+mkdir %PREFIX%\Scripts\node_modules\npm
+move * %PREFIX%\Scripts\node_modules\npm
 python %RECIPE_DIR%\win_download.py
 copy %PREFIX%\Scripts\iojs.exe %PREFIX%\Scripts\node.exe
 copy %PREFIX%\Scripts\iojs.lib %PREFIX%\Scripts\node.lib
-copy bin\npm.cmd %PREFIX%\Scripts
 if errorlevel 1 exit 1
