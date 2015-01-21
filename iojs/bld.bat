e0a9c7fa42497ca8e37b4dea4ff30ed8142e8bd2
Travis Swicegood <development@domain51.com>
2015-01-20 17:22:42 -0800

    Fix typo in script

--- iojs/bld.bat
+++ iojs/bld.bat
@@ -1,6 +1,6 @@
 move node_modules %PREFIX%\Scripts
 python %RECIPE_DIR%\win_download.py
-copy %PREFIX%\Scripts\iojs.exe %PREFIX\Scripts\node.exe
-copy %PREFIX%\Scripts\iojs.lib %PREFIX\Scripts\node.lib
+copy %PREFIX%\Scripts\iojs.exe %PREFIX%\Scripts\node.exe
+copy %PREFIX%\Scripts\iojs.lib %PREFIX%\Scripts\node.lib
 copy bin\npm.cmd %PREFIX%\Scripts
 if errorlevel 1 exit 1
