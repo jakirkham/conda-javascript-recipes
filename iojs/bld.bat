a8408a54d0955c824a525f1a77c39d1f554b75b3
Travis Swicegood <development@domain51.com>
2015-01-20 17:30:55 -0800

    Switch over to using xcopy to copy the whole directory structure

--- iojs/bld.bat
+++ iojs/bld.bat
@@ -2,7 +2,7 @@ mkdir %PREFIX%\Scripts
 copy bin\npm.cmd %PREFIX%\Scripts
 mkdir %PREFIX%\Scripts\node_modules
 mkdir %PREFIX%\Scripts\node_modules\npm
-move * %PREFIX%\Scripts\node_modules\npm
+xcopy * %PREFIX%\Scripts\node_modules\npm
 python %RECIPE_DIR%\win_download.py
 copy %PREFIX%\Scripts\iojs.exe %PREFIX%\Scripts\node.exe
 copy %PREFIX%\Scripts\iojs.lib %PREFIX%\Scripts\node.lib
