393a7bc4fe0aa6dc432fcdac60ba10d0e74ad791
malev <marcosvanetta@gmail.com>
2015-02-12 18:30:45 -0600

    Bower recipe for win-64 (fix path too long error)

--- bower/bld.bat
+++ bower/bld.bat
@@ -1,8 +1,15 @@
 REM needed to ensure 2.3.0 see https://github.com/npm/npm/issues/4080
 CALL npm install -g --upgrade npm@next
+CALL npm install -g flatten-packages
 CALL npm install -g %PKG_NAME%@%PKG_VERSION%
 
-REM Remove npm so it's not accidentally packaged up
+CALL cd %PREFIX%
+CALL npm dedupe
+CALL npm shrinkwrap
+CALL flatten-packages
+
+REM Remove flatten-packages and npm so it's not accidentally packaged up
+CALL npm uninstall -g flatten-packages
 CALL npm uninstall -g npm
 
 REM Exit zero because the npm uninstall, while successful, will exit with an
