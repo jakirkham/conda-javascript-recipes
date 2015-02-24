b99c1140c433ea2e935487fefee296ace48f5499
Travis Swicegood <development@domain51.com>
2015-02-24 14:10:28 -0800

    Adjust to use fenestrate

--- bower/bld.bat
+++ bower/bld.bat
@@ -1,15 +1,14 @@
 REM needed to ensure 2.3.0 see https://github.com/npm/npm/issues/4080
 CALL npm install -g --upgrade npm@next
-CALL npm install -g flatten-packages
+CALL npm install -g fenestrate
 CALL npm install -g %PKG_NAME%@%PKG_VERSION%
 
-CALL cd %PREFIX%
-CALL npm dedupe
-CALL npm shrinkwrap
-CALL flatten-packages
+CALL cd %PREFIX%/bower
+CALL fenestrate make .
+CALL fenestrate rewrite .
 
 REM Remove flatten-packages and npm so it's not accidentally packaged up
-CALL npm uninstall -g flatten-packages
+CALL npm uninstall -g fenestrate
 CALL npm uninstall -g npm
 
 REM Exit zero because the npm uninstall, while successful, will exit with an
