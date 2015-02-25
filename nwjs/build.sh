07436edc5ff4412404fbbd1a584a734102be4b18
Travis Swicegood <development@domain51.com>
2015-02-24 18:08:56 -0800

    Make sure all the correct permissions are set

--- nwjs/build.sh
+++ nwjs/build.sh
@@ -7,10 +7,14 @@ mkdir -p $BIN
 mkdir -p $PREFIX/nwjs
 
 if [[ (`uname` == Darwin) ]]; then
-	mv $SRC_DIR/nwjs.app/* $PREFIX/nwjs
+	# Ensure permissions are perserved
+	cp -Rp $SRC_DIR/nwjs.app/* $PREFIX/nwjs
 	rm $PREFIX/nwjs/Contents/Info.plist
 
 	chmod +x $PREFIX/nwjs/Contents/MacOS/nwjs
+	chmod +x "$PREFIX/nwjs/Contents/Frameworks/nwjs Helper EH.app/Contents/MacOS/nwjs Helper EH"
+	chmod +x "$PREFIX/nwjs/Contents/Frameworks/nwjs Helper EH.app/Contents/MacOS/nwjs Helper EH"
+	chmod +x "$PREFIX/nwjs/Contents/Frameworks/nwjs Helper.app/Contents/MacOS/nwjs Helper"
 	cat <<EOF >$EXEC
 #!/bin/sh
 
@@ -20,7 +24,7 @@ EOF
 fi
 
 if [[ (`uname` == Linux) ]]; then
-	mv $SRC_DIR/* $PREFIX/nwjs
+	cp -Rp $SRC_DIR/* $PREFIX/nwjs
 	chmod +x $PREFIX/nwjs/nw
 
 	cat <<EOF >$EXEC
