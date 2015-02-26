bb7008fe8bc7d2896e9c72ccfa765899b0e45c3a
malev <marcosvanetta@gmail.com>
2015-02-25 22:45:05 -0600

    Update win_download.py for iojs 1.3.0

--- iojs/win_download.py
+++ iojs/win_download.py
@@ -4,15 +4,14 @@ from urllib import urlretrieve
 
 CHECKSUMS = {
     "x86": {
-        "iojs.exe": "47d67f1b828010a3472c84ec39b9f526e784d7c6707aa85aff371a8cf21049f8",
-        "iojs.lib": "7cbceda422a1df22760cef632d4db3b97ef9c4f508cccc494a7df8cf482ca9b3",
+        "iojs.exe": "944bfdea819c5c0d85a90963fa619e58f725a16e744abfb4a7efb7c7a3cb26a4",
+        "iojs.lib": "fe247fc592bc8c08eae1a3d2a3cce3cec385451acd724a05313895bd15875838",
     },
 
     "x64": {
-        "iojs.exe": "114b92aa8061dae6b8443694bbba2aabb100aff81ab6508064afddc5af237eb0",
-        "iojs.lib": "95ff61556461cb8f864d9958889e9387cd58961e466c1f73b67203acefbf2c26",
+        "iojs.exe": "0a856b86a3fdc7eb0fd0649218c00acee88db6e7fbb49f7790b2ce2c8acfca5a",
+        "iojs.lib": "ab42ae57eb63722811b6b47be673e6add23a1b0a7ab1de043f7a06bd61e4abde",
     }
-
 }
 
 ARCH = {
@@ -20,7 +19,7 @@ ARCH = {
     "64": "x64",
 }[os.environ["ARCH"]]
 
-ROOT_URL = "https://iojs.org/dist/v1.0.3/win-{}/".format(ARCH)
+ROOT_URL = "https://iojs.org/dist/v1.3.0/win-{}/".format(ARCH)
 FILES = ["iojs.exe", "iojs.lib"]
 TARGET_DIR = os.path.join(os.environ["PREFIX"], "Scripts")
 if not os.path.exists(TARGET_DIR):
