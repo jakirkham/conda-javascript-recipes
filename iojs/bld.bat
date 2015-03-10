015081936d0fa1757570f795caf95f6b3dea63e4
Travis Swicegood <development@domain51.com>
2015-03-10 11:23:51 -0700

    Fix issues with installation
    * Fixes issue where paths include spaces
    * Fixes issue where variables were resolved at build instead of run time
    * Switches out to explicitly calling iojs.exe instead of relying on var

--- iojs/bld.bat
+++ iojs/bld.bat
@@ -1,8 +1,8 @@
-mkdir %PREFIX%\Scripts
-copy bin\npm.cmd %PREFIX%\Scripts
-mkdir %PREFIX%\Scripts\node_modules
-mkdir %PREFIX%\Scripts\node_modules\npm
-xcopy /E * %PREFIX%\Scripts\node_modules\npm
-python %RECIPE_DIR%\win_download.py
-ECHO @"%~dp0\iojs.exe" %%* >> %PREFIX%\Scripts\node.cmd
+mkdir "%PREFIX%\Scripts"
+copy bin\npm.cmd "%PREFIX%\Scripts"
+mkdir "%PREFIX%\Scripts\node_modules"
+mkdir "%PREFIX%\Scripts\node_modules\npm"
+xcopy /E * "%PREFIX%\Scripts\node_modules\npm"
+python "%RECIPE_DIR%\win_download.py"
+ECHO @"%PREFIX%\Scripts\iojs.exe" %%* >> "%PREFIX%\Scripts\node.cmd"
 if errorlevel 1 exit 1
