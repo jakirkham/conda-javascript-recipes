8c9d3523a7a45615bd90320c765e9937a076e10a
Travis Swicegood <development@domain51.com>
2015-03-10 11:46:06 -0700

    Switch back to using current directory to avoid bug in conda
    Reference: https://github.com/conda/conda/issues/1187

--- iojs/bld.bat
+++ iojs/bld.bat
@@ -4,5 +4,5 @@ mkdir "%PREFIX%\Scripts\node_modules"
 mkdir "%PREFIX%\Scripts\node_modules\npm"
 xcopy /E * "%PREFIX%\Scripts\node_modules\npm"
 python "%RECIPE_DIR%\win_download.py"
-ECHO @"%PREFIX%\Scripts\iojs.exe" %%* >> "%PREFIX%\Scripts\node.cmd"
+ECHO @"%%~dp0\iojs.exe" %%* >> "%PREFIX%\Scripts\node.cmd"
 if errorlevel 1 exit 1
